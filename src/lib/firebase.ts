084f3df7499839561db20a35b78322dc345af93e
Nicholas Battaglia <nicbattaglia@oasisevcs.com>
2025-09-08 20:28:06 +0000

    Ive named the database to connect to "theme-journal-database" in firebas

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -18,6 +18,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
-const db = getFirestore(app);
+const db = getFirestore(app, "theme-journal-database");
 
 export { app, auth, db };
