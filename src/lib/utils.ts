143470a02a4b47d3f686b06a9e834ea826b3f106
Nicholas Battaglia <nicbattaglia@oasisevcs.com>
2025-09-08 21:17:55 +0000

    nothing changed

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,4 @@
+
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
@@ -7,6 +8,10 @@ export function cn(...inputs: ClassValue[]) {
 
 export function hexToRgba(hex: string, opacity: number): string {
     const hexValue = hex.replace('#', '');
+    if (hexValue.length !== 6) {
+        console.error("Invalid hex color:", hex);
+        return `rgba(0, 0, 0, ${opacity})`;
+    }
     const r = parseInt(hexValue.substring(0, 2), 16);
     const g = parseInt(hexValue.substring(2, 4), 16);
     const b = parseInt(hexValue.substring(4, 6), 16);
